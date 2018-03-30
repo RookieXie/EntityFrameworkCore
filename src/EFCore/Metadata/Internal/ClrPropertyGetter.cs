1141e163a72845718d8faaa8ef6aa04c910667e7
AndriySvyryd <Andriy.Svyryd@microsoft.com>
2018-03-29 18:16:04 -0700

    Improve ChangeTracking performance
    Fixes #11194

--- src/EFCore/Metadata/Internal/ClrPropertyGetter.cs
+++ src/EFCore/Metadata/Internal/ClrPropertyGetter.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Runtime.CompilerServices;
 using JetBrains.Annotations;
 
 namespace Microsoft.EntityFrameworkCore.Metadata.Internal
@@ -14,20 +15,30 @@ namespace Microsoft.EntityFrameworkCore.Metadata.Internal
         where TEntity : class
     {
         private readonly Func<TEntity, TValue> _getter;
+        private readonly Func<TEntity, bool> _hasDefaultValue;
 
         /// <summary>
         ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        public ClrPropertyGetter([NotNull] Func<TEntity, TValue> getter)
+        public ClrPropertyGetter([NotNull] Func<TEntity, TValue> getter, [NotNull] Func<TEntity, bool> hasDefaultValue)
         {
             _getter = getter;
+            _hasDefaultValue = hasDefaultValue;
         }
 
         /// <summary>
         ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        public virtual object GetClrValue(object instance) => _getter((TEntity)instance);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public object GetClrValue(object instance) => _getter((TEntity)instance);
+
+        /// <summary>
+        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool HasDefaultValue(object instance) => _hasDefaultValue((TEntity)instance);
     }
 }
